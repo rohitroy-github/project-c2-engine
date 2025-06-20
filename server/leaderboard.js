8d3c9c24199add4b9e55c0084f71a9eeb295f7a9
rohitroy@github <rhtry.tech@gmail.com>
2025-06-20 15:34:43 +0530

    add: testing-logs

--- server/leaderboard.js
+++ server/leaderboard.js
@@ -5,8 +5,29 @@ function getLeaderboard() {
     .map(([name, data]) => ({
       name,
       pnl: data.pnl,
+      holdings: Object.entries(data.holdings)
+        .filter(([_, qty]) => qty > 0)
+        .map(([symbol, qty]) => ({
+          symbol,
+          quantity: parseFloat(qty.toFixed(4))  // limit decimal places for display
+        }))
     }))
     .sort((a, b) => b.pnl - a.pnl);
 }
 
 module.exports = { getLeaderboard };
+
+
+// ### PNL with just the name and PNL value
+// const { users } = require('./users');
+
+// function getLeaderboard() {
+//   return Object.entries(users)
+//     .map(([name, data]) => ({
+//       name,
+//       pnl: data.pnl,
+//     }))
+//     .sort((a, b) => b.pnl - a.pnl);
+// }
+
+// module.exports = { getLeaderboard };
