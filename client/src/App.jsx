ce9bb427e833c04af3b02c058677997a0ec1e18d
rohitroy@github <rhtry.tech@gmail.com>
2025-06-29 18:22:34 +0530

    add: base client-side setup with tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center p-5">
+        <p className="font-montserrat text-center text-5xl">This is the initial setup for the trade simulator</p>
+      </div>
+    </>
+  )
+}
+
+export default App
