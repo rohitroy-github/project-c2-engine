131a0f1a92af5e8e2617948f7b16a57ff4da9895
thecodermaniac <aritra.mondal.tech@gmail.com>
2025-07-16 23:12:31 +0530

    conditional rendenring enabled for navbar items added logout button added protected routes for auto re-routing changes vite config for real time changes

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -2,33 +2,41 @@ import { Link } from "react-router-dom";
 import { useAuthContext } from "../context/authContext";
 
 export default function Navbar() {
-  const { userInfo } = useAuthContext();
+  const { userInfo, updateAuth } = useAuthContext();
   return (
     <nav className="bg-white shadow-md p-4 flex items-center justify-between font-montserrat">
       <div className="flex gap-6 text-gray-800 font-semibold text-sm md:text-base">
-        <Link to="/register" className="hover:text-purple-600 transition">
+        {!userInfo?.username && <Link to="/register" className="hover:text-purple-600 transition">
           Register
-        </Link>
-        <Link to="/login" className="hover:text-purple-600 transition">
+        </Link>}
+        {!userInfo?.username && <Link to="/login" className="hover:text-purple-600 transition">
           Login
-        </Link>
-        <Link to="/trade" className="hover:text-purple-600 transition">
+        </Link>}
+        {userInfo?.username && <Link to="/trade" className="hover:text-purple-600 transition">
           Trade
-        </Link>
+        </Link>}
         <Link to="/leaderboard" className="hover:text-purple-600 transition">
           Leaderboard
         </Link>
-                <Link to="/dashboard" className="hover:text-purple-600 transition">
+        {userInfo?.username && <Link to="/dashboard" className="hover:text-purple-600 transition">
           Dashboard
-        </Link>
+        </Link>}
       </div>
-
-      {userInfo?.username && (
-        <div className="flex items-center justify-end gap-3 bg-indigo-600 text-white font-bold px-4 py-2 rounded-md shadow-md">
-          <span>{userInfo.username}</span>
-          <span>₹{userInfo.inr.toFixed(2)} INR</span>
-        </div>
-      )}
+      <div className="flex items-center gap-4">
+        {userInfo?.username && (
+          <div className="flex items-center justify-end gap-3 bg-indigo-600 text-white font-bold px-4 py-2 rounded-md shadow-md">
+            <span>{userInfo.username}</span>
+            <span>₹{userInfo.inr.toFixed(2)} INR</span>
+          </div>
+        )}
+        {userInfo?.username && (
+          <button className="flex items-center justify-end gap-3 bg-yellow-100 text-yellow-700 font-bold px-4 py-2 rounded-md shadow-md cursor-pointer" onClick={() => {
+            updateAuth(null);
+          }}>
+            Logout
+          </button>
+        )}
+      </div>
     </nav>
   );
 }
